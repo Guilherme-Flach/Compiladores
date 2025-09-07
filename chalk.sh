48fa32a872b49b1a6214566a50849aee932f9a09
Guilherme Flach <gui.ismael.flach@gmail.com>
2025-09-07 18:46:09 -0300

    Add auto export to cli command

--- chalk.sh
+++ chalk.sh
@@ -2,23 +2,49 @@
 COMMAND=$1
 STAGE=$2
 
+STAGE_FOLDER="etapa-$STAGE"
+
 case $COMMAND in 
   run)
-  cd "etapa-$STAGE"
+  cd $STAGE_FOLDER
   make
   ./etapa$STAGE
   ;;
 
   test)
-  cd "etapa-$STAGE"
+  cd $STAGE_FOLDER
   make test
   bash -c "./tests/test"
+  ;;
+
+  export)
+  # Create workdir
+  rm -r .tmp
+  mkdir .tmp
+
+  # Export relevant files
+  cp $STAGE_FOLDER/Makefile .tmp
+  cp $STAGE_FOLDER/main.c .tmp
+  cp $STAGE_FOLDER/parser.y .tmp
+  cp $STAGE_FOLDER/scanner.l .tmp
+  cd .tmp
+  tar cvzf etapa$STAGE.tgz .
+
+  # Test if our export is OK
+  rm testDir
+  mkdir testDir
+  cp etapa$STAGE.tgz testDir
+  cd testDir
+  tar -xzf etapa$STAGE.tgz
+  make
+  ./etapa$STAGE
   ;;
 
   *)
   echo "Unkown command `$COMMAND`! Available commands are
   run stageNumber
-  test stageNumber"
+  test stageNumber
+  export stageNumber"
   exit 1
   ;;
 esac
