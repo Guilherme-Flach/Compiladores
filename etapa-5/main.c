59ce36a83e4e9e6f9b9d4db2d39811e9b17cb418
Eduarda Tessari Pereira <dudatessari@gmail.com>
2025-11-23 13:41:34 -0300

    fix problem with empty file

--- etapa-5/main.c
+++ etapa-5/main.c
@@ -14,9 +14,12 @@ asd_tree_t *arvore = NULL;
 int main(int argc, char **argv) {
   int ret = yyparse();
   // asd_print_graphviz(arvore);
-  print_operation_list(arvore->code);
-  destroy_operation_list(arvore->code);
-  asd_free(arvore);
+  if(arvore != NULL){
+    print_operation_list(arvore->code);
+    destroy_operation_list(arvore->code);
+    asd_free(arvore);
+  }
+
   yylex_destroy();
   return ret;
 }
